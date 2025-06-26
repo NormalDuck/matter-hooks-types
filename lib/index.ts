95e517883b056fe3ebdf6e9144476873cf51312d
Jun Jie Huang <1juhuang2@gmail.com>
2025-06-26 11:22:37 -0700

    fix: require matter hooks

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 //we are not exporting the main package here, just the hooks
 // import "./package/Matter";
 import "./package/Matter"
+import "./package/matter-hooks"
 
 export * from "./package/matter-hooks";
--- out/index.d.ts
+++ out/index.d.ts
@@ -1,2 +1,3 @@
 // import "./package/Matter";
+// import "./package/matter-hooks";
 export * from "./package/matter-hooks";
